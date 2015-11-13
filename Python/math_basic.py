4b3306c3e845a61f34bb60a2072f42fae0a579b6
Tutorial Doctor <TutorialDoctor@users.noreply.github.com>
2015-11-12 19:37:37 -0500

    Create math_basic.py

---
+++ Python/math_basic.py
@@ -0,0 +1,21 @@
+# Get the sum of two numbers a and b
+def sum(a,b):
+  return a+b
+
+# Get the difference of two numbers a and b
+def difference(a,b):
+  return a-b
+
+# Get the quotient of two numbers a and b where b cannot equal 0
+# You have to use a float somewhere in your dividion so that the answer comes out as a float
+def quotient(a,b):
+  if b != 0:
+    return a/float(b)
+
+# Get the product of two numbers:
+def product(a,b):
+  return a*b
+  
+# That's it for basic math in a computer program!
+# No cheating on your homework! Hehe ;)
+
