fdffa5c4c0c329a81d7cbe1f2a1438bfd8bca278
Tutorial Doctor <TutorialDoctor@users.noreply.github.com>
2015-11-12 19:41:57 -0500

    Update math_basic.py

--- Python/math_basic.py
+++ Python/math_basic.py
@@ -8,6 +8,7 @@ def difference(a,b):
 
 # Get the quotient of two numbers a and b where b cannot equal 0
 # You have to use a float somewhere in your dividion so that the answer comes out as a float
+# A float is a number with a decimal
 def quotient(a,b):
   if b != 0:
     return a/float(b)
